13bceb344b51bd513eae83aa2651e55480001021
Florent Nisseron <florent.nisseron@open-groupe.com>
2020-04-27 17:45:18 +0200

    -

--- mapstore-addon/epics/signalement.js
+++ mapstore-addon/epics/signalement.js
@@ -1,19 +1,19 @@
 const Rx = require('rxjs');
-const axios = require('../libs/ajax');
+const axios = require('../../../web/client/libs/ajax');
 const uuidv1 = require('uuid/v1');
 const {actions, loadAttachmentConfiguration} = require('../actions/signalement');
 const assign = require('object-assign');
 
 module.exports = (config) => ({
-    newAnnotationEpic: (action$) => action$.ofType(actions.ATTACHMENT_CONFIGURATION_LOAD)
+    loadAttachmentConfigurationEpic: (action$) => action$.ofType(actions.ATTACHMENT_CONFIGURATION_LOAD)
         .switchMap((action) => {
         	if (action.attachmentConfiguration) {
-                return Rx.Observable.of(fieldsLoaded(action.layer, action.layer.thematic.fields)).delay(0);
+                return Rx.Observable.of(loadAttachmentConfiguration(action.attachmentConfiguration)).delay(0);
             }
             const url = "/reporting/attachment/configuration"
             return Rx.Observable.defer(() => axios.get(url))
-                .switchMap((response) => Rx.Observable.of(fieldsLoaded(action.layer, config.readFields(response.data))))
-                .catch(e => Rx.Observable.of(fieldsError(action.layer, e)));
+                .switchMap((response) => Rx.Observable.of(loadAttachmentConfiguration(config.readFields(response.data))))
+                .catch(e => Rx.Observable.of(loadError("Failed to load attachment configuration", e)));
         })
 
 });
