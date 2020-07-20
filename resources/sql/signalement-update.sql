5601782d5eaca379003310e8eba3914a2720fec1
Florent Nisseron <florent.nisseron@open-groupe.com>
2020-07-20 15:03:22 +0200

    creation image docker

--- resources/sql/signalement-update.sql
+++ resources/sql/signalement-update.sql
@@ -1,4 +1,9 @@
 
+INSERT INTO section_definition (name,label,definition) values ('section-test', 'Test', '');
+INSERT INTO form_definition( name) VALUES ('simpleProcces_draft');
+INSERT INTO form_section_definition ( read_only, order_, section_definition_id, form_definition_id) values ( false, 0, currval('section_definition_id_seq'), currval('form_definition_id_seq'));
+insert into process_form_definition ( process_definition_id, revision, user_task_id, form_definition_id) values ( 'simpleGroupReportingProcess', null, 'draft', currval('form_definition_id_seq') );
+
 INSERT INTO section_definition (name,label,definition) values ('section-simple-comment', 'Commentaire', '');
 INSERT INTO form_definition( name) VALUES ('simpleProcces_userTask_1');
 INSERT INTO form_section_definition ( read_only, order_, section_definition_id, form_definition_id) values ( false, 0, currval('section_definition_id_seq'), currval('form_definition_id_seq'));
