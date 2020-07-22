2abbaa30e1ce57bcdbbc05ef1c67aaca3e4c07c1
Florent Nisseron <florent.nisseron@open-groupe.com>
2020-07-22 08:33:27 +0200

    suppression duplicate code

--- georchestra-signalement-core/src/main/java/org/georchestra/signalement/core/entity/styling/ProcessStylingEntity.java
+++ georchestra-signalement-core/src/main/java/org/georchestra/signalement/core/entity/styling/ProcessStylingEntity.java
@@ -12,7 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import org.georchestra.signalement.core.common.LongId;
+import org.georchestra.signalement.core.common.AbstractProcessLinkEntity;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -27,7 +27,7 @@ import lombok.ToString;
 @ToString
 @Entity
 @Table(name = "process_styling")
-public class ProcessStylingEntity implements LongId {
+public class ProcessStylingEntity extends AbstractProcessLinkEntity {
 
 	@Id
 	@Column(name = "id")
@@ -47,54 +47,4 @@ public class ProcessStylingEntity implements LongId {
 	@JoinColumn(name = "styling_id")
 	private StylingEntity styling;
 
-	@Override
-	@SuppressWarnings({"common-java:DuplicatedBlocks"})
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-		result = prime * result + ((getProcessDefinitionId() == null) ? 0 : getProcessDefinitionId().hashCode());
-		result = prime * result + ((getRevision() == null) ? 0 : getRevision().hashCode());
-		result = prime * result + ((getUserTaskId() == null) ? 0 : getUserTaskId().hashCode());
-		return result;
-	}
-
-	@Override
-	@SuppressWarnings({"squid:S3776","common-java:DuplicatedBlocks"})
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (!(obj instanceof ProcessStylingEntity)) {
-			return false;
-		}
-		ProcessStylingEntity other = (ProcessStylingEntity) obj;
-		if (getId() != null && getId().equals(other.getId())) {
-			return true;
-		}
-		if (getProcessDefinitionId() == null) {
-			if (other.getProcessDefinitionId() != null) {
-				return false;
-			}
-		} else if (!getProcessDefinitionId().equals(other.getProcessDefinitionId())) {
-			return false;
-		}
-		if (getRevision() == null) {
-			if (other.getRevision() != null) {
-				return false;
-			}
-		} else if (!getRevision().equals(other.getRevision())) {
-			return false;
-		}
-		if (getUserTaskId() == null) {
-			if (other.getUserTaskId() != null) {
-				return false;
-			}
-		} else if (!getUserTaskId().equals(other.getUserTaskId())) {
-			return false;
-		}
-		return true;
-	}
-	
-	
 }
