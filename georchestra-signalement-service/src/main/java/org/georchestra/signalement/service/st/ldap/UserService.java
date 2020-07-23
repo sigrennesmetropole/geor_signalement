6fce1778526df404ab9ab69b26ccafc967b70f7a
Florent Nisseron <florent.nisseron@open-groupe.com>
2020-07-23 15:10:02 +0200

    Fix/addRoles On getMe AI method

--- georchestra-signalement-service/src/main/java/org/georchestra/signalement/service/st/ldap/UserService.java
+++ georchestra-signalement-service/src/main/java/org/georchestra/signalement/service/st/ldap/UserService.java
@@ -16,12 +16,17 @@ public interface UserService {
 
 	/**
 	 * Retourne l'utilisateur courant stocké dans le security context
-	 * @return l'utilisateur courant
+	 * 
+	 * @see UserService.getUserByLogin
+	 * @return l'utilisateur courant avec ses rôles
 	 */
 	User getMe();
 
 	/**
-	 * Retoure un utilisateur par son login
+	 * Retoure un utilisateur par son login. Attention les informations sont
+	 * extraites du LDAP. Mais cette méthode ne peut pas extraire les rôles du User
+	 * correspondant
+	 * 
 	 * @param login
 	 * @return
 	 */
@@ -29,12 +34,15 @@ public interface UserService {
 
 	/**
 	 * Retourne les contexts visibles par l'utilisateur courant
+	 * 
 	 * @return la liste des contextes
 	 */
 	List<ContextDescription> getVisibleContexts();
 
 	/**
-	 * Retourne les contexts visibles par l'utilisateur dont le login est passé en paramètre
+	 * Retourne les contexts visibles par l'utilisateur dont le login est passé en
+	 * paramètre
+	 * 
 	 * @param login
 	 * @return la liste des contextes
 	 */
