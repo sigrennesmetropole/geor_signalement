1d28faca3c9cb400ef8a43f04012dd6ba7c85126
NAKOUBAN RAOUL CAMARA <nakouban-raoul.camara@open-groupe.com>
2021-06-14 11:23:13 +0200

    Mise à jour du qervice de recuperation des communes

--- georchestra-signalement-core/src/main/java/org/georchestra/signalement/core/dao/acl/GeographicAreaDao.java
+++
@@ -1,8 +0,0 @@
-package org.georchestra.signalement.core.dao.acl;
-
-import org.georchestra.signalement.core.dao.QueryDslDao;
-import org.georchestra.signalement.core.entity.acl.GeographicAreaEntity;
-
-public interface GeographicAreaDao  extends QueryDslDao<GeographicAreaEntity, Long> {
-
-}
