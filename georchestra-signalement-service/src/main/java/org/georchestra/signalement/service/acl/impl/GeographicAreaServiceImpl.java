7afbd7bb41bde31a522cac311ff28ce54367a024
NAKOUBAN RAOUL CAMARA <nakouban-raoul.camara@open-groupe.com>
2021-06-22 14:35:12 +0000

    Resolve CDCRM-785 "Correctif fournir info zone geographique"

--- georchestra-signalement-service/src/main/java/org/georchestra/signalement/service/acl/impl/GeographicAreaServiceImpl.java
+++ georchestra-signalement-service/src/main/java/org/georchestra/signalement/service/acl/impl/GeographicAreaServiceImpl.java
@@ -8,12 +8,16 @@ import org.georchestra.signalement.core.entity.acl.GeographicAreaEntity;
 import org.georchestra.signalement.service.acl.GeographicAreaService;
 import org.georchestra.signalement.service.mapper.acl.GeographicAreaMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class GeographicAreaServiceImpl implements GeographicAreaService {
+
+    @Value("${geographic_area.rennes_metropole}")
+    private String rennesMetropole;
 
     @Autowired
     private GeographicAreaCustumDao geographicAreaCustumDao;
@@ -24,7 +28,7 @@ public class GeographicAreaServiceImpl implements GeographicAreaService {
     @Override
     public List<GeographicArea> searchGeographicAreaIntersectWithGeometry(Geometry geometry, GeographicType geographicType) {
         // On cherche d'abord l'id de la geographic Area
-        List<GeographicAreaEntity> geographicAreaEntities = geographicAreaCustumDao.searchGeographicAreaIntersectWithGeometry(geometry, geographicType);
+        List<GeographicAreaEntity> geographicAreaEntities = geographicAreaCustumDao.searchGeographicAreaIntersectWithGeometry(geometry, geographicType, rennesMetropole);
         // On convertie les entités en dtos
         return geographicAreaMapper.entitiesToDtos(geographicAreaEntities);
     }
