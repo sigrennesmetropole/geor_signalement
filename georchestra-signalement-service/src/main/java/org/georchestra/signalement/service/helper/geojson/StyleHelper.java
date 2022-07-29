64318eadc96dd108f168f6f579699516f4acfaf7
Benjamin BOUVIER <BBO22368@open-groupe.com>
2022-07-29 16:09:50 +0200

    update file to make easier the conversion between a style and the JSON description

--- georchestra-signalement-service/src/main/java/org/georchestra/signalement/service/helper/geojson/StyleHelper.java
+++ georchestra-signalement-service/src/main/java/org/georchestra/signalement/service/helper/geojson/StyleHelper.java
@@ -1,9 +1,19 @@
 package org.georchestra.signalement.service.helper.geojson;
 
+import org.georchestra.signalement.core.dto.GeographicType;
 import org.georchestra.signalement.core.dto.Style;
+import org.georchestra.signalement.core.dto.StyleContainer;
+import org.georchestra.signalement.core.entity.styling.StylingEntity;
+import org.georchestra.signalement.service.mapper.acl.StylingMapper;
+import org.georchestra.signalement.service.mapper.acl.StylingMapperImpl;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StyleHelper {
 
@@ -18,6 +28,15 @@ public class StyleHelper {
     private static final String FILLCOLOR = "fillColor";
     private static final String FILLOPACITY = "fillOpacity";
     private static final String DASHARRAY = "dashArray";
+
+    //Pattern in order to detect in a string a dashArray, example : ,"dashArray":[0.2,3]
+    private static final Pattern dashArrayPattern = Pattern.compile(",\\\"dashArray\\\":\\[((([0-9]*(\\.[0-9]*)?(,[0-9]*(\\.[0-9]*)?)))|(null))\\]");
+
+    //Pattern in order to detect in a string an iconAnchor example : "iconANCHOR":[0.2,3]?
+    private static final Pattern iconAnchorPattern = Pattern.compile("\\\"iconAnchor\\\":\\[((([0-9]*(\\.[0-9]*)?(,[0-9]*(\\.[0-9]*)?)))|(null))\\],");
+    //The comma could become a problem if the order of data are updated
+
+    private StylingMapper styleMapper = new StylingMapperImpl();
 
 
     /**
@@ -232,5 +251,142 @@ public class StyleHelper {
 
 
         return style;
+    }
+
+    private String getTypeFromEntity(StylingEntity entity){
+        JSONObject description = new JSONObject(entity.getDefinition());
+        return getTypeFromJson(description);
+    }
+    private String getTypeFromJson(JSONObject description){
+        return description.keys().hasNext() ? description.keys().next() : null;
+    }
+
+    /** Intern parsing and mapping function for this special case
+     * Problem solved : The data in the database are mostly JSON and could have different formats.
+     * We need to parse them and then map them.  Default mapper cannot map all data.
+     */
+    public StylingEntity mappingStyleToEntity(StyleContainer styleContainer) {
+
+        StylingEntity res = styleMapper.dtoToEntity(styleContainer);
+        JSONArray dashArray = null;
+        JSONArray iconAnchor = null;
+        String type = styleContainer.getType().toString();
+
+        //In case of type is a Polygon or a Line we need to remove the arrays before JsonObject conversion
+        if(type == "POLYGON"){
+            dashArray = new JSONArray(styleContainer.getStyle().getDashArray());
+            styleContainer.getStyle().setDashArray(null);
+        }
+
+        if(type == "LINE"){
+            iconAnchor = new JSONArray(styleContainer.getStyle().getIconAnchor());
+            styleContainer.getStyle().setIconAnchor(null);
+        }
+
+
+        JSONObject description = new JSONObject(styleContainer.getStyle());
+        JSONObject arr = new JSONObject();
+
+        //Final description which follow the type
+        JSONObject descriptionType = new JSONObject();
+        switch (type){
+            case "POINT":
+                Style defaultPointStyle = createDefaultStylePoint();
+                descriptionType.put("iconGlyph",description.has("iconGlyph")&& !description.getString("iconGlyph").equals("")?description.get("iconGlyph"):defaultPointStyle.getIconGlyph());
+                descriptionType.put("iconShape",description.has("iconShape")&& !description.getString("iconShape").equals("")?description.get("iconShape"):defaultPointStyle.getIconShape());
+                descriptionType.put("iconColor",description.has("iconColor")&& !description.getString("iconColor").equals("")?description.get("iconColor"):defaultPointStyle.getIconColor());
+                break;
+
+            case "LINE":
+                Style defaultLineStyle = createDefaultLineStyle();
+                descriptionType.put("filtering",description.has("filtering")?description.get("filtering"):null);
+                descriptionType.put("weight",description.has("weight")?description.get("weight"):defaultLineStyle.getWeight());
+                descriptionType.put("opacity",description.has("opacity")?description.get("opacity"):defaultLineStyle.getFillColor());
+                if(iconAnchor.toList().contains(null)){
+                    descriptionType.put("iconAnchor",defaultLineStyle.getIconAnchor());
+                }else{
+                    descriptionType.put("iconAnchor",iconAnchor);
+                }
+                descriptionType.put("color",description.has("color")&& !description.getString("color").equals("")?description.get("color"):defaultLineStyle.getColor());
+                break;
+
+            case "POLYGON":
+                Style defaultPolygonStyle = createDefaultPolygonStyle();
+                descriptionType.put("weight",description.has("weight")?description.get("weight"):defaultPolygonStyle.getWeight());
+                descriptionType.put("fillColor",description.has("fillColor")&& !description.getString("color").equals("")?description.get("fillColor"):defaultPolygonStyle.getFillColor());
+                descriptionType.put("fillOpacity",description.has("fillOpacity")?description.get("fillOpacity"):defaultPolygonStyle.getFillOpacity());
+                if(dashArray.toList().contains(null)){
+                    descriptionType.put("dashArray",defaultPolygonStyle.getDashArray());
+                }else{
+                    descriptionType.put("dashArray",dashArray);
+                }
+                descriptionType.put("opacity",description.has("opacity")?description.get("opacity"):defaultPolygonStyle.getOpacity());
+                descriptionType.put("color",description.has("color")&& !description.getString("color").equals("")?description.get("color"):defaultPolygonStyle.getColor());
+                break;
+        }
+        arr.put(type, descriptionType);
+        res.setDefinition(arr.toString());
+        return res;
+    }
+
+    public StyleContainer mappingStyleToDto(StylingEntity entity){
+        Style style = new Style();
+
+        String def = entity.getDefinition();
+        String dashArrayValues = "";
+        String iconAnchorValues = "";
+
+        Matcher m = dashArrayPattern.matcher(def);
+        Matcher m2 = iconAnchorPattern.matcher(def);
+        if (m2.find()) {
+            String iconAnchorString = m2.group(0);
+            def = def.replace(iconAnchorString,"");
+            iconAnchorValues = m2.group(1);
+        }else if(m.find()) {
+            String dashArrayString = m.group(0);
+            def = def.replace(dashArrayString,"");
+            dashArrayValues = m.group(1);
+        }
+        JSONObject description = new JSONObject(def);
+        String type = description.keys().hasNext() ? description.keys().next() : null;
+
+
+        //Map with the default mapper
+        StyleContainer res = styleMapper.entityToDto(entity);
+        //Create a style from the type of the style (Line, Point or Polygon)
+        if (GeographicType.fromValue(type) == GeographicType.POLYGON) {
+
+            style = StyleHelper.createPolygonStyle(def);
+            List<Double> dashArray = new LinkedList<>();
+            for (String value : dashArrayValues.split(",")){
+                try {
+                    dashArray.add(Double.parseDouble(value));
+                }catch (NumberFormatException e){
+                    dashArray.add(null);
+                }
+            }
+            style.setDashArray(dashArray);
+
+        } else if (GeographicType.fromValue(type) == GeographicType.LINE) {
+
+
+            style = (StyleHelper.createLineStyle(def));
+            List<Double> iconAnchor = new LinkedList<>();
+            for (String value : iconAnchorValues.split(",")){
+                try {
+                    iconAnchor.add(Double.parseDouble(value));
+                }catch (NumberFormatException e){
+                    iconAnchor.add(null);
+                }
+            }
+            style.setIconAnchor(iconAnchor);
+
+        } else if (GeographicType.fromValue(type) == GeographicType.POINT) {
+            style = (StyleHelper.createPointStyle(entity.getDefinition()));
+        }
+        //Finnish mapping style and type from StyleContainer
+        res.setStyle(style);
+        res.setType(GeographicType.valueOf(type));
+        return res;
     }
 }
