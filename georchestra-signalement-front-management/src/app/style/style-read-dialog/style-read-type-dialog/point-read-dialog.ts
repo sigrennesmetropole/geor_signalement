81044a6c84fe4de91ea1f4b7df9e5755e06172d8
Benjamin BOUVIER <BBO22368@open-groupe.com>
2022-07-29 15:59:13 +0200

    front for read part of style

---
+++ georchestra-signalement-front-management/src/app/style/style-read-dialog/style-read-type-dialog/point-read-dialog.ts
@@ -0,0 +1,46 @@
+import {AfterViewInit, Component, Inject} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {TranslateService} from "@ngx-translate/core";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {ToasterUtil} from "../../../utils/toaster.util";
+/**
+ * @title Dialog Overview
+ */
+
+
+ @Component({
+   selector: 'point-read-dialog',
+   templateUrl: 'point-read-dialog.html',
+   styleUrls: ['point-read-dialog.scss'],
+ })
+
+/**
+  * The dialog window to add a context
+  */
+export class PointReadDialog implements AfterViewInit{
+    public iconGlyphControl = new FormControl('', [Validators.required]);
+    public iconColorControl = new FormControl('', [Validators.required]);
+    public iconShapeControl = new FormControl('', [Validators.required]);
+
+    /*public StyleReadForm = new FormGroup({
+        iconGlyph: this.iconGlyphControl,
+        iconColor: this.iconColorControl,
+        iconShape: this.iconShapeControl,
+    });*/
+
+
+    constructor(
+        private dialogRef: MatDialogRef<PointReadDialog>,
+        private toaster: ToasterUtil,
+        private translateService: TranslateService,
+        @Inject(MAT_DIALOG_DATA) private data:any
+    )
+    {
+        this.iconGlyphControl.setValue(this.data.target.style.iconGlyph);
+        this.iconColorControl.setValue(this.data.target.style.iconColor);
+        this.iconShapeControl.setValue(this.data.target.style.iconShape);
+    }
+
+    ngAfterViewInit(): void {
+    }
+}
