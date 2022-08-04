54775e94125feeddafb74b2e896a7644241a07a0
Benjamin BOUVIER <BBO22368@open-groupe.com>
2022-08-04 10:34:40 +0200

    remove unused import

--- georchestra-signalement-core/src/main/java/org/georchestra/signalement/core/dao/styling/StylingDao.java
+++ georchestra-signalement-core/src/main/java/org/georchestra/signalement/core/dao/styling/StylingDao.java
@@ -1,7 +1,6 @@
 package org.georchestra.signalement.core.dao.styling;
 
 import org.georchestra.signalement.core.dao.QueryDslDao;
-import org.georchestra.signalement.core.entity.acl.RoleEntity;
 import org.georchestra.signalement.core.entity.styling.StylingEntity;
 import org.springframework.stereotype.Repository;
 
