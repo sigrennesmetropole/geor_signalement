f7c52de41d7ffa0d55cd94b3d6a2b000a0f97d9e
fni18300 <fni18300@open-groupe.com>
2022-08-08 10:35:30 +0200

    TU de parsing

---
+++ georchestra-signalement-service/src/test/java/org/georchestra/signalement/service/helper/StyleHelperTest.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.georchestra.signalement.service.helper;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.georchestra.signalement.StarterSpringBootTestApplication;
+import org.georchestra.signalement.service.helper.geojson.StyleHelper;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ * @author FNI18300
+ *
+ */
+@SpringBootTest(classes = StarterSpringBootTestApplication.class)
+@ComponentScan({ "org.georchestra.signalement.api", "org.georchestra.signalement.service",
+		"org.georchestra.signalement.core" })
+@TestPropertySource(value = { "classpath:signalement.properties", "classpath:signalement-common.properties" })
+class StyleHelperTest {
+	
+	@Test
+	void testParseJSON() {
+		JSONObject object = StyleHelper.getDefinitionStyleObj("{\"p\": [ 12, 5 ]}");
+		assertNotNull(object);
+		
+		object = StyleHelper.getDefinitionStyleObj("{\"POLYGON\":{\"fillColor\":\"test\",\"fillOpacity\":0.5,\"color\":\"test2\",\"weight\":1,\"dashArray\":[0,2],\"opacity\":0.5}}");
+		assertNotNull(object);
+	}
+	
+	
+}
