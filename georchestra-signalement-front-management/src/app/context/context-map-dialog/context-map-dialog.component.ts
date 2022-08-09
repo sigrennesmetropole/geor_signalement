de3b3458239562602230b9c1b127e34e0749b1ea
Benjamin BOUVIER <BBO22368@open-groupe.com>
2022-08-09 15:44:58 +0200

    added a simple map in the dialog

--- georchestra-signalement-front-management/src/app/context/context-map-dialog/context-map-dialog.component.ts
+++ georchestra-signalement-front-management/src/app/context/context-map-dialog/context-map-dialog.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import TileLayer from "ol/layer/Tile";
+import {OSM} from "ol/source";
+import {fromLonLat} from "ol/proj";
+import {View} from "ol";
+import Map from 'ol/Map';
 
 @Component({
-  selector: 'app-context-map-dialog',
+  selector: 'context-map-dialog',
   templateUrl: './context-map-dialog.component.html',
   styleUrls: ['./context-map-dialog.component.scss']
 })
 export class ContextMapDialog implements OnInit {
 
-  constructor() { }
+  map ?: Map;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.initMap()
+  }
+
+  initMap() {
+    this.map = new Map({
+      target: 'mapRef',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([-1.651, 48.119], 'EPSG:3857'),
+        zoom: 11,
+        projection: 'EPSG:3857'
+      })
+    });
   }
 
 }
