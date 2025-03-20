d5c034eaad10a9e3b276f70ce0ae0fe76708f746
FLORENT NISSERON <florent.nisseron@open-groupe.com>
2025-03-20 09:02:02 +0100

    Edit push-to-github.sh

--- ci/push-to-github.sh
+++ ci/push-to-github.sh
@@ -34,7 +34,7 @@ if [ -z "$GIT_TOKEN" ]; then
   exit 1
 fi
 
-DESTINATION_REPOSITORY=/tmp/geor_signalement/
+DESTINATION_REPOSITORY=/tmp/geor_signalement
 [ -z "$GIT_REMOTE" ] && GIT_REMOTE="https://$GITHUB_RM_ACCOUNT:$GIT_TOKEN@github.com/sigrennesmetropole/geor_signalement.git"
 [ -z "$PRODUCTION_REMOTE_BRANCH" ] && PRODUCTION_REMOTE_BRANCH="master"
 TEMP_DIRECTORY=/tmp
@@ -50,11 +50,14 @@ cd -
 echo "Syncing source $SOURCE_REPOSITORY to target $DESTINATION_REPOSITORY..."
 
 # Backup .git (git back Jojo !) directory from destination (deleted by "--delete-excluded")
-mv "$DESTINATION_REPOSITORY/.git" "$TEMP_DIRECTORY/.git.back"
+mv "${DESTINATION_REPOSITOR}/.git" "${TEMP_DIRECTORY}/.git.back"
+
+ls -a "${DESTINATION_REPOSITOR}/.git"
+ls -a "${TEMP_DIRECTORY}/.git.back"
 
 rsync \
   --archive \
-  --exclude-from="$SOURCE_REPOSITORY/.gitignore" \
+  --exclude-from="${SOURCE_REPOSITORY}/.gitignore" \
   --filter="merge push-to-github-filter.txt" \
   --delete-excluded \
   --delete-during \
@@ -62,13 +65,17 @@ rsync \
   "$SOURCE_REPOSITORY" "$DESTINATION_REPOSITORY"
 
 # Restore .git directory to destination
-mv "$TEMP_DIRECTORY/.git.back" "$DESTINATION_REPOSITORY/.git"
+mv "${TEMP_DIRECTORY}/.git.back" "${DESTINATION_REPOSITORY}/.git"
+
+ls -al "${DESTINATION_REPOSITORY}/.git"
 
 # Commit and push
-cd "$DESTINATION_REPOSITORY" || exit 1
+cd "$DESTINATION_REPOSITORY"
 echo "Committing and pushing to https://github.com/sigrennesmetropole/geor_signalement.git..."
+
 echo "`pwd`"
 ls -a
+
 git config user.name "$GITHUB_RM_ACCOUNT"
 git config user.email "$GITHUB_RM_LOGIN"
 git add --all
